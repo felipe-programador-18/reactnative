849a89c3b665f91e4f76ebd657e61e3feaa4b0dd
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-28 19:06:26 -0300

    c11- practice quite react native

--- flexbox/App.js
+++ flexbox/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import logo from './assests/nature.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.box}>
       <Text>Remember and practice more and more always</Text>
+      <Image style={styles.img} source={logo} />
       </View>
       <View style={styles.box}>
        <Text> The Years gonna be my!!!</Text>
@@ -26,11 +29,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   box:{
-    padding:30,
+    //padding:30,
+    flex:1,
     backgroundColor:'grey',
     margin:4,
     color:'white',
     textAlign: 'center',
   },
-  
+  img:{
+    width:365,
+    position:'relative',
+    height: 100
+    }  
 });
