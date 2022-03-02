f08f7686685ae1ed2318603ccedc6b11d8102981
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-02 09:02:17 -0300

    c13- adding new somethigs

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+import logo from './assets/img.jpg'
 
 export default function App() {
       
@@ -23,14 +25,20 @@ export default function App() {
         
        <View style = {styles.square}>
        <Text style = {styles.zero}> {num} </Text>
+      
        <Button style={styles.one} title='-' onPress={Decrement} />
        <Button style={styles.two} title='+' onPress={Increment} />
+         
+       
         </View>
 
        <View>
-        <Text style= {styles.footer} >Developer Felipe Martins Programer 18</Text>
+       <View>
+            
+        <Text style= {styles.footer} >Developer Fe-Prog-18</Text>
        </View> 
-      <StatusBar style="auto" />
+     
+    </View>
     </View>
   );
 }
