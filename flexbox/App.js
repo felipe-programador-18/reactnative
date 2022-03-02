8a60adb24726709ff2b394ea2b9544301faf341c
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-02 08:40:14 -0300

    c12- adding practice about reactnative

--- flexbox/App.js
+++ flexbox/App.js
@@ -6,14 +6,15 @@ import logo from './assests/nature.jpg'
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
+
+      <View style={styles.boxone}>
       <Text>Remember and practice more and more always</Text>
       <Image style={styles.img} source={logo} />
       </View>
-      <View style={styles.box}>
+      <View style={styles.boxtwo}>
        <Text> The Years gonna be my!!!</Text>
       </View>
-      <View style={styles.box}>
+      <View style={styles.boxthree}>
       <Text>My working strugling going be rewards!!!</Text>
       </View>
      
@@ -28,17 +29,38 @@ const styles = StyleSheet.create({
     alignItems:'stretch' ,
     justifyContent: 'space-between',
   },
-  box:{
+  boxone:{
     //padding:30,
     flex:1,
     backgroundColor:'grey',
+    alignItems:'center',
+    height:80,
     margin:4,
     color:'white',
     textAlign: 'center',
   },
+  boxtwo:{
+    //padding:30,
+    flex:2,
+    backgroundColor:'grey',
+    height:80,
+    margin:4,
+    color:'white',
+    textAlign: 'center',
+  },
+  boxthree:{
+    //padding:30,
+    flex:3,
+    backgroundColor:'grey',
+    height:80,
+    margin:4,
+    color:'white',
+    textAlign: 'center',
+  },
+
   img:{
-    width:365,
+    width:200,
     position:'relative',
-    height: 100
+    height:80
     }  
 });
